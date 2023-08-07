91b1b27aaa00e088fdcf63fd148b744c7654d180
ProgrammerShipon <msshipon234@gmail.com>
2023-08-07 22:08:54 +0600

    show button

--- src/Sections/HeroBanner.jsx
+++ src/Sections/HeroBanner.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from '../Components/Button';
 
 const HeroBanner = () => {
    return (
      <div id="hero-banner" className="h-screen">
        <h1 className="text-3xl text-pink-700"> This is Hero Banner Route </h1>
+       <Button> Start Here </Button>
      </div>
    );
 };
