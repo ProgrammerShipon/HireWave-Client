4668aee6d07eb3bedf0be87665e808974c4bde1f
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-08-08 12:42:14 +0600

    test First Commit rayhan

---
+++ src/Sections/TopCategories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopCategories = () => {
+    return (
+        <div>
+            <h1>This is category</h1>
+        </div>
+    );
+};
+
+export default TopCategories;
