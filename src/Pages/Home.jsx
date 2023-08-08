89425a8f990b9339ad56475cd85ad5e2d16faadc
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-08-08 17:12:49 +0600

    added reviews section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroBanner from '../Sections/HeroBanner';
+import Reviews from '../Sections/Reviews/Reviews';
 
 const Home = () => {
    return (
      <>
        <HeroBanner />
+       <Reviews/>
      </>
    );
 };
