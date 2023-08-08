ce0fe7dca333079f73adb2f3f9d3481016bd56b8
ProgrammerSiam <programmersiam.com@gmail.com>
2023-08-08 23:12:07 +0600

    some modify

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import HeroBanner from '../Sections/HeroBanner';
+import React from "react";
+import HeroBanner from "../Sections/HeroBanner";
+import TopEmployee from "../Sections/TopEmployee";
+import TopRecruiters from "../Sections/TopRecruiters";
 
 const Home = () => {
-   return (
-     <>
-       <HeroBanner />
-     </>
-   );
+  return (
+    <>
+      <HeroBanner />
+      <TopEmployee />
+      <TopRecruiters />
+    </>
+  );
 };
 
 export default Home;
