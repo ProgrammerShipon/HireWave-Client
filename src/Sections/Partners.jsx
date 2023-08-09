1ddfd40e43cee782854e4a6e6007f09b3e5299f6
ProgrammerShipon <msshipon234@gmail.com>
2023-08-09 11:32:19 +0600

    compuny data

--- src/Sections/Partners.jsx
+++ src/Sections/Partners.jsx
@@ -1,31 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Partners = () => {
- console.log(isLoadingPartner, partnerData);const [partnerData, setData] = useState(null);
- const [isLoadingPartner, setLoadingPartner] = useState(true);
+  const [partnerData, setData] = useState(null);
 
- useEffect(() => {
-   fetch("OurPartners.json")
-     .then((res) => res.json())
-     .then((data) => {
-       setData(data);
-       setLoadingPartner(false);
-     });
- }, []);
+  // Todo: Partner Fetching Data 
+  useEffect(() => {
+    fetch("OurPartners.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
 
-   return (
-     <section>
-       <div className="container">
-         <div className="section-title">
-           <p> Our Partners </p>
-           <h2>
-             {" "}
-             We Have Worked with <span className="text-green"> 10,000+ </span> Trusted Companies 
-           </h2>
-         </div>
-       </div>
-     </section>
-   );
+  console.log(partnerData);
+
+  return (
+    <section>
+      <div className="container">
+        {/* TODO:  Section Title */}
+
+        <div className="flex flex-wrap items-center justify-center gap-6">
+          {partnerData &&
+            partnerData.map((partner) => (
+              <figure className="max-w-[150px] hover:">
+                <img src={partner?.logoImg} alt={partner?.name} />
+              </figure>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Partners;
