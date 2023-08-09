fb5a34b9cd03054ee6331c7fe3c1d83275ce4291
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-08-09 23:18:51 +0600

    updated footer folder structure and added comment

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -8,7 +8,11 @@ const Footer = () => {
   return (
     <footer className='text-white bg-dark mt-10'>
       <div className='container'>
+
+        {/* Top footer part */}
         <div className='grid grid-cols-1 md:grid-cols-3 gap-20 pt-12'>
+
+          {/* Logo, address, and social media links */}
           <div className='col-span-1'>
             <img className='w-1/2' src={logo} alt="" />
             <p className='mt-5 mb-2'>43/7 Northern Tower, Banani, Dhaka-1263</p>
@@ -28,54 +32,63 @@ const Footer = () => {
             </div>
           </div>
 
-          <section className='grid grid-cols-2 lg:grid-cols-4 gap-10 col-span-1 md:col-span-2'>
+          {/* Footer links */}
+          <div className='grid grid-cols-2 lg:grid-cols-4 gap-10 col-span-1 md:col-span-2'>
+
+            {/* Job seekers */}
             <div>
               <h1 className='font-semibold text-emerald-400 mb-3'>Job seekers</h1>
               <ul>
-                <li className='mb-2'><Link href="">Browse Jobs</Link></li>
-                <li className='mb-2'><Link href="">Salary Tools</Link></li>
-                <li className='mb-2'><Link href="">Career Advice</Link></li>
-                <li className='mb-2'><Link href="">Student Career Center</Link></li>
-                <li className='mb-2'><Link href="">Resume writing services</Link></li>
-                <li className='mb-2'><Link href="">Direct Contracts</Link></li>
+                <li className='mb-2'><Link to="">Browse Jobs</Link></li>
+                <li className='mb-2'><Link to="">Salary Tools</Link></li>
+                <li className='mb-2'><Link to="">Career Advice</Link></li>
+                <li className='mb-2'><Link to="">Student Career Center</Link></li>
+                <li className='mb-2'><Link to="">Resume writing services</Link></li>
+                <li className='mb-2'><Link to="">Direct Contracts</Link></li>
               </ul>
             </div>
+
+            {/* Employers */}
             <div>
               <h1 className='font-semibold text-emerald-400 mb-3'>Employers</h1>
               <ul>
-                <li className='mb-2'><Link href="">How to Hire</Link></li>
-                <li className='mb-2'><Link href="">Talent Marketplace</Link></li>
-                <li className='mb-2'><Link href="">Hire an Agency</Link></li>
-                <li className='mb-2'><Link href="">Hire worldwide</Link></li>
-                <li className='mb-2'><Link href="">Direct Contracts</Link></li>
+                <li className='mb-2'><Link to="">How to Hire</Link></li>
+                <li className='mb-2'><Link to="">Talent Marketplace</Link></li>
+                <li className='mb-2'><Link to="">Hire an Agency</Link></li>
+                <li className='mb-2'><Link to="">Hire worldwide</Link></li>
+                <li className='mb-2'><Link to="">Direct Contracts</Link></li>
               </ul>
             </div>
+
+            {/* Resources */}
             <div>
               <h1 className='font-semibold text-emerald-400 mb-3'>Resources</h1>
               <ul>
-                <li className='mb-2'><Link href="">Help and Support</Link></li>
-                <li className='mb-2'><Link href="">Success Stories</Link></li>
-                <li className='mb-2'><Link href="">Blogs</Link></li>
-                <li className='mb-2'><Link href="">Researches</Link></li>
-                <li className='mb-2'><Link href="">Community</Link></li>
-                <li className='mb-2'><Link href="">Events and Webinars</Link></li>
-                <li className='mb-2'><Link href="">News and press</Link></li>
+                <li className='mb-2'><Link to="">Help and Support</Link></li>
+                <li className='mb-2'><Link to="">Success Stories</Link></li>
+                <li className='mb-2'><Link to="">Blogs</Link></li>
+                <li className='mb-2'><Link to="">Researches</Link></li>
+                <li className='mb-2'><Link to="">Community</Link></li>
+                <li className='mb-2'><Link to="">Events and Webinars</Link></li>
+                <li className='mb-2'><Link to="">News and press</Link></li>
               </ul>
             </div>
+
+            {/* Company */}
             <div>
               <h1 className='font-semibold text-emerald-400 mb-3'>Company</h1>
               <ul>
-                <li className='mb-2'><Link href="">About Us</Link></li>
-                <li className='mb-2'><Link href="">Leadership</Link></li>
-                <li className='mb-2'><Link href="">Careers</Link></li>
-                <li className='mb-2'><Link href="">Our Impact</Link></li>
-                <li className='mb-2'><Link href="">Contact Us</Link></li>
+                <li className='mb-2'><Link to="">About Us</Link></li>
+                <li className='mb-2'><Link to="">Leadership</Link></li>
+                <li className='mb-2'><Link to="">Careers</Link></li>
+                <li className='mb-2'><Link to="">Our Impact</Link></li>
+                <li className='mb-2'><Link to="">Contact Us</Link></li>
               </ul>
             </div>
-          </section>
+          </div>
         </div>
 
-
+        {/* Bottom footer part */}
         <div className='md:flex justify-center gap-10 mt-10 pb-5 text-center'>
 
           <div>
