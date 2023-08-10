a9ea9033f9748a303261aa7688c0697ec81d981a
ProgrammerShipon <msshipon234@gmail.com>
2023-08-11 00:12:31 +0600

    code added mafuj

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: ["react", "react/jsx-dev-runtime"],
+  },
   plugins: [react()],
-})
+});
