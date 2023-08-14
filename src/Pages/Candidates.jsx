4ca0d8128e4194ae4973f08cc8de3e1233f3654b
radifkhanrafin <meherafkabir@gmail.com>
2023-08-14 22:56:03 +0600

    add Candidates searchBar

--- src/Pages/Candidates.jsx
+++ src/Pages/Candidates.jsx
@@ -1,4 +1,5 @@
 import CandidateCard from '../Components/CandidateCard';
+import CandidatesSearchBar from '../Components/CandidatesSearchBar';
 import SectionTitle from '../Components/SectionTitle';
 import useCandidatesData from '../Hooks/useCandidatesData';
 
@@ -7,7 +8,12 @@ const Candidates = () => {
     return (
         <section className='py-16 md:py-20 '>
             <div className='container'>
-            <SectionTitle title={"Our Candidates"}></SectionTitle>
+
+                <div className='pt-40 py-8 '>
+                   <CandidatesSearchBar></CandidatesSearchBar>
+                </div>
+
+                <SectionTitle title={"Our Candidates"}></SectionTitle>
                 <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8 mt-8'>
                     {
                         candidates.map(candidate =>
