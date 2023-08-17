d4176fbade694a8c6de7b69454e11404748d6308
foridislam332 <foridislam991@gmail.com>
2023-08-17 19:16:33 +0600

    reaponsive update

--- src/Sections/GetStart.jsx
+++ src/Sections/GetStart.jsx
@@ -5,17 +5,17 @@ import { AiOutlineUserAdd } from "react-icons/ai";
 
 const GetStart = () => {
     return (
-        <section className='bg-[url("https://i.ibb.co/Dg1bVQB/Breadcrumbs.jpg")] bg-fixed bg-right-top bg-no-repeat bg-cover xl:bg-center'>
+        <section className='bg-[url("https://i.ibb.co/Dg1bVQB/Breadcrumbs.jpg")] bg-fixed md:bg-right-top bg-no-repeat bg-cover xl:bg-center'>
             <div className='bg-green/70 py-20 md:py-24 duration-300'>
                 <div className='container'>
-                    <div className='max-w-4xl mx-auto flex items-center justify-between'>
+                    <div className='max-w-4xl mx-auto flex flex-col md:flex-row items-center justify-between'>
                         <div className='max-w-[600px] text-center md:text-left'>
-                            <h1 className='text-3xl mb-5 text-white'>Get Start Your New Job With Us !</h1>
+                            <h1 className='text-4xl md:text-3xl mb-5 text-white'>Get Start Your New Job With Us !</h1>
 
                             <p className='text-white font-light tracking-wide'>Ride the wave of success with Hire Wave, your dedicated partner in finding the perfect job match through our expert hiring services.</p>
                         </div>
 
-                        <div>
+                        <div className='mt-8 md:mt-0'>
                             <Link to='/login' className='text-lg bg-dark text-white hover:text-dark px-8 py-3 rounded-lg hover:bg-white duration-300 shadow-xl hover:shadow-white/30 flex items-center justify-center gap-1'>
                                 <AiOutlineUserAdd /> Create an Account
                             </Link>
