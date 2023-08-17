990055a74f5c8bfbf2f81fdf971c2f0bac271e1f
foridislam332 <foridislam991@gmail.com>
2023-08-17 22:58:00 +0600

    login & register page redesign done

--- src/Shared/SocalLogin.jsx
+++ src/Shared/SocalLogin.jsx
@@ -1,11 +1,11 @@
 
 const SocalLogin = () => {
-const handleGoogleLogin =()=>{
-    console.log("handleGoogleLogin")
-}
-const handleGithubleLogin =()=>{
-    console.log("handleGithubleLogin")
-}
+    const handleGoogleLogin = () => {
+        console.log("handleGoogleLogin")
+    }
+    const handleGithubleLogin = () => {
+        console.log("handleGithubleLogin")
+    }
 
     return (
         <div className='space-y-2 py-3'>
@@ -14,12 +14,11 @@ const handleGithubleLogin =()=>{
                 <img src="https://i.ibb.co/PrvjGn6/google-2.png" className='w-11' alt="" />
 
             </button>
+
             <button onClick={handleGithubleLogin} className='w-full flex justify-center items-center border border-green hover:shadow-md hover:shadow-green/20 p-2 h-12 rounded-md'>
                 <h3 className='text-xl text-dark font-semibold'>Continue With </h3>
                 <img src="https://i.ibb.co/jWK1zcZ/github-PNG40.png" className='w-9' alt="" />
-
             </button>
-            
         </div>
     );
 };
