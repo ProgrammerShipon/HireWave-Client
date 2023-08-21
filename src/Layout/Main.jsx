74432bd78275944ced4b927514f35354bbb6b25f
foridislam332 <foridislam991@gmail.com>
2023-08-21 17:02:07 +0600

    scroll to top done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,19 @@
 import { Outlet } from 'react-router-dom';
+import ScrollToTop from "react-scroll-to-top";
 import Footer from '../Shared/Footer';
 import Navbar from '../Shared/Navbar';
-import ScrollToTop from '../Components/ScrollToTop';
+import StayTop from '../Components/StayTop';
+
+
+// react toastify
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Main = () => {
    return (
       <>
-         <ScrollToTop />
+         <StayTop />
+         <ScrollToTop smooth color='#33e2a0' className='px-2 bg-white text-center flex items-center justify-center animate-bounce shadow-4xl shadow-gray/50' />
 
          <Navbar />
 
@@ -19,7 +24,7 @@ const Main = () => {
          <Footer />
 
          <ToastContainer />
-      </>
+      </ >
    );
 };
 
