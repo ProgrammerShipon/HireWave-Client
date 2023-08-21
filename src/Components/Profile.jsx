eb75464145fe6a7ef47fe2f1ed7e09b71c623ffb
foridislam332 <foridislam991@gmail.com>
2023-08-21 16:38:42 +0600

    notification & message ui added

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,20 +1,17 @@
 import { Link } from 'react-router-dom';
 
-// react icons
-import { IoMdNotificationsOutline } from 'react-icons/io';
-import { MdOutlineMail } from 'react-icons/md';
 import useAuth from '../Hooks/useAuth';
+import Message from './Message';
+import Notification from './Notification';
 
 const Profile = () => {
     const { user, logOut } = useAuth();
     return (
         <div className='flex items-center gap-3'>
-            <button className='w-10 h-10 flex items-center justify-center text-green bg-green/10 rounded-full'>
-                <IoMdNotificationsOutline size='24' />
-            </button>
-            <button className='w-10 h-10 flex items-center justify-center text-green bg-green/10 rounded-full'>
-                <MdOutlineMail size='24' />
-            </button>
+            <div className='flex items-center'>
+                <Notification />
+                <Message />
+            </div>
 
             <div className="relative z-[999] group">
                 <div
@@ -42,7 +39,7 @@ const Profile = () => {
 
                 {/* Dropdown */}
                 <div
-                    className="absolute right-0 top-28 max-w-xs min-w-[200px] bg-white shadow-lg shadow-green/20 rounded-lg origin-top-right transition-all duration-300 ease-in-out group-hover:top-[60px] p-4 invisible group-hover:visible opacity-0 group-hover:opacity-100">
+                    className="absolute right-0 top-28 max-w-xs min-w-[200px] bg-white shadow-4xl shadow-gray/40 origin-top-right transition-all duration-300 ease-in-out group-hover:top-[60px] p-4 invisible group-hover:visible opacity-0 group-hover:opacity-100">
 
                     <ul
                         className="flex flex-col gap-1 text-dark"
@@ -50,15 +47,15 @@ const Profile = () => {
                         <li>
                             <Link to='/my_profile' className='hover:text-green duration-300'>My Profile</Link>
                         </li>
-                        {/* <li>
+                        <li>
                             <Link to='/events' className='hover:text-green duration-300'>Events</Link>
-                        </li> */}
+                        </li>
+                        <li>
+                            <Link to='/contact' className='hover:text-green duration-300'>Contact</Link>
+                        </li>
                         <li>
                             <Link to='/learning' className='hover:text-green duration-300'>Learning</Link>
                         </li>
-                        {/* <li>
-                            <Link to='/contact' className='hover:text-green duration-300'>Contact</Link>
-                        </li> */}
                         <li>
                             <Link to='/dashboard' className='hover:text-green duration-300'>Dashboard</Link>
                         </li>
