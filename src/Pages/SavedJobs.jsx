73c6ac0ea5e7ebb5c0cd542df9589ba608151d85
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-08-22 10:29:32 +0600

    saved jobs page

--- src/Pages/SavedJobs.jsx
+++ src/Pages/SavedJobs.jsx
@@ -1,8 +1,16 @@
 import { Helmet } from "react-helmet";
 import Breadcrumbs from "../Components/Breadcrumbs";
 import Divider from "../Components/Divider";
+import useAllJobs from "../Hooks/useAllJobs";
+import SectionTitle from "../Components/SectionTitle";
+import { Link } from "react-router-dom";
+import Button from "../Components/Button";
+import FeaturedJobCard from "../Components/FeaturedJobCard";
+import SavedJobsCard from "./SavedJobsCard";
+import { IoIosArrowDown } from "react-icons/io";
 
 const SavedJobs = () => {
+    const [allJobsData] = useAllJobs();
     return (
         <>
             {/* page title */}
@@ -15,6 +23,21 @@ const SavedJobs = () => {
             {/* section here */}
             {/* border */}
             <Divider />
+            <section className="py-20 md:py-[120px] duration-300">
+                <div className="container">
+
+
+                    {/* Saved jobs content */}
+                    <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-5 lg:gap-0 pt-12 md:pt-16 lg:max-w-4xl mx-auto duration-300 relative">
+                        {allJobsData.length > 0 &&
+                            allJobsData?.slice(0, 4)
+                                .map((job, index) => <SavedJobsCard key={index} job={job} />)}
+                    </div>
+
+                </div>
+            </section>
+
+
         </>
     );
 };
---
+++ src/Pages/SavedJobsCard.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+// react icons
+import { FaRegHeart } from 'react-icons/fa';
+import GetAgoTime from '../Components/GetAgoTime';
+import { Link } from 'react-router-dom';
+import { IoIosArrowDown } from 'react-icons/io';
+import Button from '../Components/Button';
+
+
+const SavedJobsCard = ({ job }) => {
+    const { companyLogo, title, companyName, postedDate, location, jobType, industry, salary, skills } = job;
+    return (
+        <div className="relative w-full bg-white flex items-center justify-between border lg:border-0 lg:border-b border-green/50 lg:last:border-transparent hover:shadow-3xl lg:hover:border-white rounded-lg lg:rounded-none hover:rounded-lg overflow-hidden scale-100 lg:hover:scale-105 clear-both hover:z-20 px-5 py-8 lg:py-6 lg:pr-0 duration-300 group">
+            <Button className="bg-green text-white py-2 px-4 rounded-lg absolute top-2 left-20 z-10">
+                Apply Now
+            </Button>
+
+            <div className="flex flex-col sm:flex-row items-center gap-10">
+                {/* company logo */}
+                <div className="w-44 h-44 pt-5 lg:p-6 overflow-hidden">
+                    <img className="w-full object-cover object-center" src={companyLogo} alt={companyName} />
+                </div>
+
+                {/* card content */}
+                <div>
+                    <p className="text-green lg:text-purple bg-green/20 px-4 py-1 absolute lg:static top-2 left-2 rounded-md inline-block lg:mb-1 shadow-md shadow-green/10 capitalize">{jobType}</p>
+
+                    <Link to='/' className="text-xl md:text-2xl text-dark group-hover:text-green duration-300 sm:mr-3 lg:mr-0 sm:line-clamp-1 max-w-lg">{title}</Link>
+
+                    <h5 className="mt-2 hidden lg:block duration-300">{companyName} - <span className="text-gray">
+                        <GetAgoTime datetime={postedDate} />
+                    </span></h5>
+
+                    <div className="flex flex-col lg:flex-row lg:items-center text-center sm:text-left lg:gap-8 mt-1 duration-300">
+                        <p>location: <span className="text-gray">{location}</span></p>
+
+                        <p className="hidden lg:block duration-300">Industry: <span className="text-gray">{industry}</span></p>
+
+                        <p>Salary: <span className="text-gray">${salary}/month</span></p>
+                    </div>
+
+
+                    <div className="flex flex-wrap items-center justify-center sm:justify-start gap-2 mt-4 lg:mt-2 duration-300">
+                        {
+                            skills?.map((item, index) => <p
+                                key={index}
+                                className="bg-green/20 hover:bg-white text-green px-3 py-[2px] shadow-lg shadow-green/10 hover:shadow-dark/20 rounded-md cursor-pointer duration-300"
+                            >{item}</p>)
+                        }
+
+                    </div>
+
+
+                </div>
+            </div>
+
+            {/* save button */}
+
+            {/* Apply Now Button */}
+            <Button className="bg-green  text-white py-2 px-4 rounded-lg mr-4">
+                Apply Now
+            </Button>
+
+
+
+
+
+
+
+        </div>
+    );
+};
+
+export default SavedJobsCard;
