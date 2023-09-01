606a3ac6035c29ac4cbab907740fbc01005c7137
foridislam332 <foridislam991@gmail.com>
2023-09-01 08:29:30 +0600

    few update

--- src/Dashboard/ManageUsers.jsx
+++ src/Dashboard/ManageUsers.jsx
@@ -1,7 +1,12 @@
 import DashTitle from "../Components/DashComponents/DashTitle";
 import ManageUserTableRow from "../Components/DashComponents/ManageUserTableRow";
 import useCandidatesData from "../Hooks/useCandidatesData";
+import CountUp from 'react-countup';
 
+// react icons
+import { LiaIndustrySolid, LiaUserClockSolid } from 'react-icons/lia';
+import { PiUsersThreeLight } from 'react-icons/pi';
+import { CgUserList } from 'react-icons/cg';
 
 const ManageUsers = () => {
     const [candidatesData, loading] = useCandidatesData();
@@ -10,8 +15,72 @@ const ManageUsers = () => {
         <section className='m-5 rounded-md'>
             <DashTitle title='Manage Users' />
 
-            <div className="mt-10 w-full overflow-x-auto duration-300 rounded-md shadow-4xl shadow-gray/40 bg-white">
-                {/* table */}
+            {/* top card */}
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-14'>
+
+                {/* total users */}
+                <div className='bg-white shadow-4xl shadow-gray/40 rounded-md px-3 group'>
+                    <div className='flex items-center justify-between border-b border-[#18025B]/40 mb-2'>
+                        <div className='w-16 h-16 bg-[#18025B] text-white flex items-center justify-center rounded-lg shadow-xl shadow-[#18025B]/50 -mt-10 group-hover:-mt-14 duration-300'>
+                            <PiUsersThreeLight size='36' />
+                        </div>
+                        <div className='text-right pt-1 pb-2'>
+                            <span className='text-[#18025B] text-4xl drop-shadow-xl count__up'>
+                                <CountUp duration={3} end={324} />
+                            </span>
+                            <h3 className='text-dark tracking-wider drop-shadow-xl'>Total Users</h3>
+                        </div>
+                    </div>
+                </div>
+
+                {/* recruiters */}
+                <div className='bg-white shadow-4xl shadow-gray/40 rounded-md px-3 group'>
+                    <div className='flex items-center justify-between border-b border-purple/40 mb-2'>
+                        <div className='w-16 h-16 bg-purple text-white flex items-center justify-center rounded-lg shadow-xl shadow-purple/50 -mt-10 group-hover:-mt-14 duration-300'>
+                            <LiaIndustrySolid size='36' />
+                        </div>
+                        <div className='text-right pt-1 pb-2'>
+                            <span className='text-purple text-4xl drop-shadow-xl count__up'>
+                                <CountUp duration={3} end={124} />
+                            </span>
+                            <h3 className='text-dark tracking-wider drop-shadow-xl'>Recruiters</h3>
+                        </div>
+                    </div>
+                </div>
+
+                {/* candidates */}
+                <div className='bg-white shadow-4xl shadow-gray/40 rounded-md px-3 group'>
+                    <div className='flex items-center justify-between border-b border-green/40 mb-2'>
+                        <div className='w-16 h-16 bg-green text-white flex items-center justify-center rounded-lg shadow-xl shadow-green/50 -mt-10 group-hover:-mt-14 duration-300'>
+                            <CgUserList size='36' />
+                        </div>
+                        <div className='text-right pt-1 pb-2'>
+                            <span className='text-green text-4xl drop-shadow-xl count__up'>
+                                <CountUp duration={3} end={135} />
+                            </span>
+                            <h3 className='text-dark tracking-wider drop-shadow-xl'>Candidates</h3>
+                        </div>
+                    </div>
+                </div>
+
+                {/* pending user */}
+                <div className='bg-white shadow-4xl shadow-gray/40 rounded-md px-3 group'>
+                    <div className='flex items-center justify-between border-b border-[#FF9671]/40 mb-2'>
+                        <div className='w-16 h-16 bg-[#FF9671] text-white flex items-center justify-center rounded-lg shadow-xl shadow-[#FF9671]/50 -mt-10 group-hover:-mt-14 duration-300'>
+                            <LiaUserClockSolid size='36' />
+                        </div>
+                        <div className='text-right pt-1 pb-2'>
+                            <span className='text-[#FF9671] text-4xl drop-shadow-xl count__up'>
+                                <CountUp duration={3} end={35} />
+                            </span>
+                            <h3 className='text-dark tracking-wider drop-shadow-xl'>Pending</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* table */}
+            <div className="mt-7 w-full overflow-x-auto duration-300 rounded-md shadow-4xl shadow-gray/40 bg-white">
                 {
                     !loading ? <table className="table lg:w-full w-[800px] text-left">
                         <thead className="text-lg text-green border-b border-green/40">
--- src/Dashboard/RecruiterList.jsx
+++ src/Dashboard/RecruiterList.jsx
@@ -9,8 +9,50 @@ const RecruiterList = () => {
         <section className='m-5 rounded-md'>
             <DashTitle title='Recruiter List' />
 
-            <div className="mt-10 w-full overflow-x-auto duration-300 rounded-md shadow-4xl shadow-gray/40 bg-white">
-                {/* table */}
+            {/* filtering option */}
+            <div className="flex flex-col lg:flex-row items-center gap-3 justify-between bg-white p-4 rounded-md shadow-xl mt-10">
+                {/* search by title */}
+                <div className="w-full sm:w-72">
+                    <input
+                        type="text"
+                        placeholder="Search recruiter name"
+                        className="border border-gray/40 p-2 rounded-md bg-white focus:outline-none focus:border-green w-full"
+                    />
+                </div>
+
+                <div className="flex flex-col sm:flex-row items-center gap-3 w-full sm:w-auto">
+                    <h2 className="text-lg text-dark">Filter By: </h2>
+
+                    {/* filter by status */}
+                    <select
+                        name="status"
+                        className="py-1 border border-gray/40 text-lightGray focus:outline-none focus:border-green rounded-md px-2 w-full sm:w-auto"
+                    >
+                        <option value="">Status</option>
+                        <option value="approve">Approve</option>
+                        <option value="pending">Pending</option>
+                        <option value="suspend">Suspend</option>
+                    </select>
+
+                    {/* filter by Industry */}
+                    <select
+                        name="Industry"
+                        className="py-1 border border-gray/40 text-lightGray focus:outline-none focus:border-green rounded-md px-2 w-full sm:w-auto"
+                    >
+                        <option value="">Industry</option>
+                        {
+                            Array.from(new Set(recruiterData.map(item => item.industry))).map((industry, index) => (
+                                <option key={index} value={industry}>
+                                    {industry}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </div>
+            </div>
+
+            {/* table */}
+            <div className="mt-6 w-full overflow-x-auto duration-300 rounded-md shadow-4xl shadow-gray/40 bg-white">
                 {
                     !loading ? <table className="table lg:w-full w-[800px] text-left">
                         <thead className="text-lg text-green border-b border-green/40">
