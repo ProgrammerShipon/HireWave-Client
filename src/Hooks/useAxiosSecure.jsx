a65439fc7f2f1ee8c19fd088342e25735c04560b
Shipon Hossen Raju <msshipon234@gmail.com>
2023-09-08 16:42:55 +0600

    bugs fixing

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,14 +1,10 @@
 import { useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 // import useAuth from './useAuth';
 
-// serverLink = "https://hire-wave-server.vercel.app/api";
-// serverLink = "http://localhost:3030/api";
-
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:3030/api',
-    // baseURL: 'https://hire-wave-server.vercel.app/api/',
+    // baseURL: 'http://localhost:3030/api',
+    baseURL: 'https://hire-wave-server.vercel.app/api/',
 });
 
 const useAxiosSecure = () => {
@@ -22,17 +18,6 @@ const useAxiosSecure = () => {
             }
             return config;
         });
-
-        // axiosSecure.interceptors.response.use(
-        //     (response) => response,
-        //     async (error) => {
-        //         if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-        //             await logOut();
-        //             navigate('/login');
-        //         }
-        //         return Promise.reject(error);
-        //     }
-        // );
     }, []);
 
     return [axiosSecure];
