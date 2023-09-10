c060e7ec7cff771a177f1ece025f2f5d591b7a1d
foridislam332 <foridislam991@gmail.com>
2023-09-10 18:45:20 +0600

    save job update

--- src/Sections/SavedJob.jsx
+++ src/Sections/SavedJob.jsx
@@ -2,6 +2,8 @@ import PageLoader from '../Components/PageLoader';
 import SavedJobsCard from '../Components/SavedJobsCard';
 import useMySavedJobs from '../Hooks/useMySavedJobs';
 
+import heartImg from '../Assets/heart.gif';
+import { Link } from 'react-router-dom';
 const SavedJob = () => {
     const [mySavedJobs, loading, refetch] = useMySavedJobs();
     if (loading) {
@@ -11,6 +13,16 @@ const SavedJob = () => {
     return (
         <section className="py-20 md:py-[120px] duration-300">
             <div className="container">
+                {
+                    !mySavedJobs.length > 0 && <div className='-mt-36 text-center'>
+                        <div className='h-96 mx-auto w-fit -z-10 relative'>
+                            <img src={heartImg} alt="heart" />
+                        </div>
+
+                        <h2 className='mt-4 text-xl font-medium max-w-lg mx-auto'>Keep track of jobs you're interested in. Select the heart icon on a job post to save it for later.</h2>
+                        <Link to='/browse_jobs' className='mt-6 block text-purple underline'>Browse Jobs</Link>
+                    </div>
+                }
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 duration-300">
 
                     {
