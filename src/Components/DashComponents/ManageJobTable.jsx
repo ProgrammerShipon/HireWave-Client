46a087fa45259a568a8cea5bf986c5ac569efa67
Shipon Hossen Raju <msshipon234@gmail.com>
2023-09-10 13:22:58 +0600

    mange user, mange job status systems complite'

--- src/Components/DashComponents/ManageJobTable.jsx
+++ src/Components/DashComponents/ManageJobTable.jsx
@@ -1,39 +1,77 @@
-import { Link } from "react-router-dom";
 import moment from "moment";
+import { Link } from "react-router-dom";
 
 // react icons
 import { FaRegStar } from "react-icons/fa";
+import { toast } from "react-toastify";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
-const ManageJobTable = ({ job }) => {
-    const { _id, title, postedDate, applied, category } = job;
-    const status = 'approve';
-    return (
-        <tr className="border-b border-green/20 hover:bg-green/10 duration-300 group">
-            <td className="px-3 py-4 font-medium text-dark w-72 md:w-[340px]">
-                <div className="flex items-center w-full">
-                    <FaRegStar className="mr-2 text-purple" />
-                    <Link to={`/job_details/${_id}`}
-                        className="font-medium group-hover:text-green duration-300 w-full line-clamp-1"
-                    >{title}</Link>
-                </div>
-            </td>
-            <td className="px-3 py-4 text-lightGray">{category}</td>
-            <td className="px-3 py-4 text-lightGray">{moment(postedDate).format("MMM Do YYYY")}</td>
-            <td className="py-3 text-center font-medium text-dark">{applied}</td>
-            <td className="px-3 py-4">
-                <div className={`relative flex gap-1 items-center justify-center px-2 rounded-full text-sm capitalize w-fit mx-auto ${status === 'approve' && 'bg-green/10 text-green font-medium shadow-lg shadow-green/20'} ${status === 'pending' && 'bg-orange-300/10 text-orange-300'} ${status === 'denied' && 'bg-red-500/10 text-red-500'}`}
-                >
-                    <select name="status" id="status" defaultValue={status}
-                        className="focus:outline-none bg-transparent"
-                    >
-                        <option value="approve">Approve</option>
-                        <option value="pending">Pending</option>
-                        <option value="denied">Denied</option>
-                    </select>
-                </div>
-            </td>
-        </tr>
-    );
+const ManageJobTable = ({ job, refetch }) => {
+  const [axiosSecure] = useAxiosSecure();
+  const { _id, title, postedDate, applied, category, status } = job;
+
+  const statusChanges = (data) => {
+    const statusData = {
+      status: data,
+    };
+
+    if (data != status) {
+      axiosSecure
+        .patch(`/allJobs/${_id}`, statusData)
+        .then((res) => {
+          if (res.status == 200) {
+            toast.success(`Update Success`);
+            refetch();
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  return (
+    <tr className="border-b border-green/20 hover:bg-green/10 duration-300 group">
+      <td className="px-3 py-4 font-medium text-dark w-72 md:w-[340px]">
+        <div className="flex items-center w-full">
+          <FaRegStar className="mr-2 text-purple" />
+          <Link
+            to={`/job_details/${_id}`}
+            className="font-medium group-hover:text-green duration-300 w-full line-clamp-1"
+          >
+            {title}
+          </Link>
+        </div>
+      </td>
+      <td className="px-3 py-4 text-lightGray">{category}</td>
+      <td className="px-3 py-4 text-lightGray">
+        {moment(postedDate).format("MMM Do YYYY")}
+      </td>
+      <td className="py-3 text-center font-medium text-dark">{applied}</td>
+      <td className="px-3 py-4">
+        <div
+          className={`relative flex gap-1 items-center justify-center px-2 rounded-full text-sm capitalize w-fit mx-auto ${
+            status === "approved" &&
+            "bg-green/10 text-green font-medium shadow-lg shadow-green/20"
+          } ${status === "pending" && "bg-orange-300/10 text-orange-300"} ${
+            status === "rejected" && "bg-red-500/10 text-red-500"
+          }`}
+        >
+          <select
+            name="status"
+            id="status"
+            defaultValue={status}
+            className="focus:outline-none bg-transparent"
+            onChange={(e) => {
+              statusChanges(e.target.value);
+            }}
+          >
+            <option value="approved">Approve</option>
+            <option value="pending">Pending</option>
+            <option value="rejected">Denied</option>
+          </select>
+        </div>
+      </td>
+    </tr>
+  );
 };
 
 export default ManageJobTable;
--- src/Components/DashComponents/ManageUserTableRow.jsx
+++ src/Components/DashComponents/ManageUserTableRow.jsx
@@ -1,37 +1,100 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 const ManageUserTableRow = ({ user }) => {
     const { _id, name, email, image, role, status } = user;
+    const [statusUpdate, setStatusUpdate] = useState();
+    const [axiosSecure] = useAxiosSecure();
+    const [isChange, setIsChange] = useState(false)
+
+    const statusChanges = () => {
+        const statusData = {
+          status: statusUpdate,
+        };
+
+        console.log("status data -> ", statusData);
+
+        if (statusData != "undefined") {
+            axiosSecure.patch(`/users/${_id}`, statusData)
+                .then(res => {
+                    console.log(res)
+                    if (res.status == 200) {
+                        toast.success(`Update Success`)
+                    }
+                })
+                .catch(err => console.log(err))
+        }
+    }
+
+    // useEffect(() => {
+    //     if (statusUpdate == status || statusUpdate == 'undefined') {
+    //       console.log(`statusUpdate == status || statusUpdate == 'undefined'`);
+    //     setIsChange(true);
+    //   } else if ( statusUpdate != 'undefined') {
+    //       console.log(`statusUpdate !== status && statusUpdate != 'undefined'`);
+    //     setIsChange(false);
+    //   }
+    // }, [statusUpdate]);
+
     return (
-        <tr className="border-b border-green/20 hover:bg-green/10 duration-300 group">
-            <td className="px-3 py-3 flex gap-2">
-                <img src={image} alt={name}
-                    className="w-12 h-12 rounded-full object-cover object-center overflow-hidden"
-                />
-                <div className="flex flex-col">
-                    <Link to={`/candidate_details/${_id}`} className="font-medium text-dark group-hover:text-green duration-300">{name}</Link>
-                    <p className="text-lightGray">{email}</p>
-                </div>
-            </td>
+      <tr className="border-b border-green/20 hover:bg-green/10 duration-300 group">
+        <td className="px-3 py-3 flex gap-2">
+          <img
+            src={image}
+            alt={name}
+            className="w-12 h-12 rounded-full object-cover object-center overflow-hidden"
+          />
+          <div className="flex flex-col">
+            <Link
+              to={`/candidate_details/${_id}`}
+              className="font-medium text-dark group-hover:text-green duration-300"
+            >
+              {name}
+            </Link>
+            <p className="text-lightGray">{email}</p>
+          </div>
+        </td>
 
-            <td className="px-3 py-4 text-center capitalize">{role}</td>
+        <td className="px-3 py-4 text-center capitalize">{role}</td>
 
-            <td className="px-3 py-4 text-center">
-                <div className={`relative flex gap-1 items-center justify-center px-2 rounded-full capitalize w-fit mx-auto ${status === 'approved' && 'bg-green/10 text-green font-medium shadow-lg shadow-green/20'} ${status === 'pending' && 'bg-orange-300/10 text-orange-300'} ${status === 'rejected' && 'bg-red-500/10 text-red-500'}`}
-                >
-                    <select name="status" id="status" defaultValue={status}
-                        className="focus:outline-none bg-transparent"
-                    >
-                        <option value="active">Approved</option>
-                        <option value="pending">Pending</option>
-                        <option value="rejected">Suspend</option>
-                    </select>
-                </div>
-            </td>
-            <td className="px-3 py-4 text-center">
-                <button className="border border-green text-dark px-4 rounded-md hover:bg-green hover:text-white duration-300">Apply</button>
-            </td>
-        </tr>
+        <td className="px-3 py-4 text-center">
+          <div
+            className={`relative flex gap-1 items-center justify-center px-2 rounded-full capitalize w-fit mx-auto ${
+              status === "approved" &&
+              "bg-green/10 text-green font-medium shadow-lg shadow-green/20"
+            } ${status === "pending" && "bg-orange-300/10 text-orange-300"} ${
+              status === "rejected" && "bg-red-500/10 text-red-500"
+            }`}
+          >
+            <select
+              onChange={(e) => {
+                setStatusUpdate(e.target.value);
+              }}
+              name="status"
+              id="status"
+              defaultValue={status}
+              className="focus:outline-none bg-transparent"
+            >
+              <option value="approved">Approved</option>
+              <option value="pending">Pending</option>
+              <option value="rejected">Suspend</option>
+            </select>
+          </div>
+        </td>
+        <td className="px-3 py-4 text-center">
+          <button
+            disabled={isChange}
+            onClick={() => statusChanges()}
+            className={`border border-green text-dark px-4 rounded-md duration-300 ${
+              isChange ? "bg-gray-300" : "hover:bg-green hover:text-white"
+            }`}
+          >
+            Apply
+          </button>
+        </td>
+      </tr>
     );
 };
 
